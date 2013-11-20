1a28166c4e6a67555b3835cf3e31c62f83e5a022
ELIN <elin@mikomoe.jp>
2013-11-20 21:19:48 +0900

    -

--- red.php
+++ red.php
@@ -1,6 +1,9 @@
 <HTML>
-<BODY>
-<FORM METHOD="<?=(preg_match("/^\/(GET|POST)\//",$_) ? $_[0] : "GET")?>" ACTION="<?=(preg_match("/^(?:\/(?:GET|POST))(https?:\/{2}.*)$/",$_) ? $m[0] : "http://127.0.0.1/")?>">
+<BODY ONLOAD="document.getElementById(1).submit()">
+<?
+	$q = urldecode(preg_replace("/\?.*$/","",getenv("REQUEST_URI")));
+?>
+<FORM ID="1" METHOD="<?=(preg_match("/\/(GET|POST)\//",$q,$_) ? $_[1] : "GET")?>" ACTION="<?=(preg_match("/(?:\/(?:GET|POST))?\/(https?:\/{2}.*)\??/",$q,$_) ? $_[1] : "http://127.0.0.1/")?>">
 <?
 foreach($_GET as $name =>$value){
 ?>
